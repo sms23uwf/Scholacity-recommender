2b92e35e2a7aeb58455877cc6ef18d15b6514811
sms23uwf <stevesatterfield2286@gmail.com>
2020-10-22 22:28:02 -0500

    now putting it back

---
+++ src/actions/courseSelections.js
@@ -0,0 +1,90 @@
+import uuid from 'uuid';
+import database from '../firebase/firebase';
+
+export const addCourseSelection = (courseselections) => ({
+  type: 'ADD_COURSE_SELECTION',
+  courseselections
+});
+
+export const startAddCourseSelection = (courseSelectionData = {}) => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    const {
+      userid = ``,
+      courseid = ``,
+      disposition = ``,
+      counter = ``,
+      rating = ``,
+      knowledgearea = ``,
+      coursename = ``,
+      coursedescription = ``,
+      instructor = ``,
+      fee = ``
+    } = courseSelectionData;
+
+    const courseUserPairing = { userid, courseid, rating, counter, rating, disposition, knowledgearea, coursename, coursedescription, instructor, fee };
+
+    return database.ref(`users_tables/${uid}/courseselection`).push({...courseUserPairing}).then((ref) => {
+      dispatch(addCourseSelection({
+        id: ref.key,
+        ...courseUserPairing
+      }));
+    });
+  };
+};
+
+// REMOVE_COURSE_SELECTION
+export const removeCourseSelection = ({ id } = {}) => ({
+  type: 'REMOVE_COURSE_SELECTION',
+  id
+});
+
+export const startRemoveCourseSelection = (selectionPairing = {}) => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    const id = selectionPairing.id;
+    
+    return database.ref(`users_tables/${uid}/courseselection/${id}`).remove().then(() => {
+      dispatch(removeCourseSelection({ id }));
+    });
+  };
+};
+
+// EDIT_COURSE_SELECTION
+export const editCourseSelection = (id, updates) => ({
+  type: 'EDIT_COURSE_SELECTION',
+  id,
+  updates
+});
+
+export const startEditCourseSelection = (id, updates) => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    return database.ref(`users_tables/${uid}/courseselection/${id}`).update(updates).then(() => {
+      dispatch(editCourseSelection(id, updates));
+    });
+  };
+};
+
+// SET_COURSE_SELECTIONS
+export const setCourseSelections = (courseselections) => ({
+  type: 'SET_COURSE_SELECTIONS',
+  courseselections
+});
+
+export const startSetCourseSelections = () => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    return database.ref(`users_tables/${uid}/courseselection`).once('value').then((snapshot) => {
+        const courseselections = [];
+
+      snapshot.forEach((childSnapshot) => {
+        courseselections.push({
+          id: childSnapshot.key,
+          ...childSnapshot.val()
+        });
+      });
+      dispatch(setCourseSelections(courseselections));
+    });
+  };
+};
